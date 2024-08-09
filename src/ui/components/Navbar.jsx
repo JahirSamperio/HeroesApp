a3ac580cf449e498b6e2d8e15a5bed29cc172ea8
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2024-08-09 00:03:08 -0600

    Login y logout funcional

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,15 +1,21 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
+
+    const {user, logout, logged} = useContext(AuthContext);
     
     const navigate = useNavigate();
 
     const onLogout = () => {
+        logout()
         navigate('/login', {
             replace: true
         })
     }
+
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
@@ -50,15 +56,9 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary'>
-                        Jahir Samperio
+                        { user?.name }
                     </span>
-
-                    <button 
-                        className='nav-item nav-link btn'
-                        onClick={onLogout}
-                    >
-                        Logout
-                    </button>
+                    <button className='nav-item nav-link btn' onClick={onLogout}>Logout</button>
                 </ul>
             </div>
         </nav>
